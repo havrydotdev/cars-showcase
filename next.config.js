986c598b1ff0ae82fdb35c2ad1f26574019095a9
Ivan <fistashkin0123@gmail.com>
2023-07-25 15:14:58 +0300

    add footer element

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = "cars-showcase";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
     output: 'export',
-    basePath: '/cars-showcase'
+    basePath: basePath,
+    assetPrefix: assetPrefix,
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
